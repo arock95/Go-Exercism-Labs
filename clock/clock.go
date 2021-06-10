7bfdb113be925e46a28159e3fbca5544e93d8e8d
arock95 <anthrock@hotmail.com>
2021-06-10 14:13:32 -0400

    updated clock to just use minutes

--- clock/clock.go
+++ clock/clock.go
@@ -3,7 +3,6 @@ package clock
 import "fmt"
 
 type clock struct {
-	hour   int
 	minute int
 }
 
@@ -12,38 +11,26 @@ type Clock clock
 
 // New creates a new clock
 func New(hour, minute int) Clock {
-	hour += minute / 60
-	minute %= 60
-
-	hour += 24
-	hour %= 24
-
-	if minute < 0 {
-		hour--
-		minute += 60
+	m := minute + hour*60
+	m %= 24 * 60
+	if m < 0 {
+		m += 24 * 60
 	}
-
-	hour %= 24
-	if hour < 0 {
-		hour += 24
-	}
-
-	return Clock{
-		hour:   hour,
-		minute: minute,
-	}
+	return Clock{m}
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	hour := c.minute / 60
+	minute := c.minute % 60
+	return fmt.Sprintf("%02d:%02d", hour, minute)
 }
 
 // Add adds minutes to the clock
 func (c Clock) Add(minutes int) Clock {
-	return New(c.hour, c.minute+minutes)
+	return New(0, c.minute+minutes)
 }
 
 // Subtract subtracts minutes from the clock
 func (c Clock) Subtract(minutes int) Clock {
-	return New(c.hour, c.minute-minutes)
+	return New(0, c.minute-minutes)
 }
